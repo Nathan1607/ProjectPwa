a8f94a1d83721c59ce0ccb6dfd2caff77b5a8ea7
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-03-28 15:46:49 +0100

    Install webmanifest + worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      const registration = await navigator.serviceWorker.register('/service-worker.ts');
+      console.log('Service Worker registered successfully:', registration);
+    } catch (error) {
+      console.log('Service Worker registration failed:', error);
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
