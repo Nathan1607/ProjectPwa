08d5c11a7cd7dbfd5dd318e3f52d65caa011529c
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-04-03 22:04:18 +0200

    Localisation ok

--- src/components/localisation/LocalisationView.tsx
+++ src/components/localisation/LocalisationView.tsx
@@ -1,16 +1,41 @@
 import { useEffect, useState } from "react";
 
+interface LocationState {
+  latitude: number;
+  longitude: number;
+}
+
 export default function LocalisationView() {
 
-  const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState<LocationState | null>(null);
 
-useEffect(() => {
-  console.log('LocalisationView mounted');
-}, []);
+  useEffect(() => {
+    if("geolocation" in navigator){
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    } else {
+      console.log('Geolocation not available');
+    }
+  }, []);
 
   return (
     <div>
-        <p>LocalisationView</p>
+        {location ? (
+        <div>
+          <p>Latitude: {location.latitude}</p>
+          <p>Longitude: {location.longitude}</p>
+        </div>
+      ) : (
+        <p>En attente de la localisation...</p>
+      )}
     </div>
   )
 }
