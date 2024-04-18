11332347050e6b6900b69f6514826726f3a74856
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-04-18 12:07:18 +0200

    Fix bug code Battery

--- src/components/BatteryView.tsx
+++ src/components/BatteryView.tsx
@@ -1,34 +1,48 @@
 import React, { useState, useEffect } from "react";
 
+interface BatteryManager extends EventTarget {
+  charging: boolean;
+  chargingTime: number;
+  dischargingTime: number;
+  level: number;
+}
+
+declare global {
+  interface Navigator {
+    getBattery(): Promise<BatteryManager>;
+  }
+}
+
 export default function BatteryStatus() {
+  const [batteryLevel, setBatteryLevel] = useState<number | null>(null);
 
-    const [batteryLevel, setBatteryLevel] = useState<number | null>(null);
-
-    useEffect(() => {
-        if ("getBattery" in navigator) {
-          navigator.getBattery().then(battery => {
+  useEffect(() => {
+    if ("getBattery" in navigator) {
+      navigator.getBattery()
+        .then(battery => {
+          updateBatteryStatus(battery);
+          battery.addEventListener("levelchange", () => {
             updateBatteryStatus(battery);
-            
-              battery.addEventListener("levelchange", () => {
-              updateBatteryStatus(battery);
-            });
           });
-        } else {
-          console.log("L'API Battery Status n'est pas disponible dans ce navigateur.");
-        }
-      }, []);
+        })
+        .catch(error => {
+          console.error("Erreur lors de l'obtention de l'état de la batterie :", error);
+        });
+    } else {
+      console.log("L'API Battery Status n'est pas disponible dans ce navigateur.");
+    }
+  }, []);
 
-      const updateBatteryStatus = (battery: Battery) => {
-        const level = battery.level * 100;
-        setBatteryLevel(level);
-      };
+  const updateBatteryStatus = (battery: BatteryManager) => {
+    const level = battery.level * 100;
+    setBatteryLevel(level);
+  };
 
   return (
     <div>
-        {batteryLevel !== null && (
-            <p>{batteryLevel}%</p>
-        )}
+      {batteryLevel !== null && (
+        <p>{batteryLevel}%</p>
+      )}
     </div>
   );
 };
-
