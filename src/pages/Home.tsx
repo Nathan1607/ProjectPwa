0848c063f3089374528b8563fa4c698f64f7f033
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-04-27 21:08:49 +0200

    Update Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,6 @@ export default function App() {
       <div>
         <Header />
         <h1>Vous êtes sur la page Home</h1>
-        <br />
-        <p>test 5</p>
-        <h1>COUCOU</h1>
       </div>
   );
 }
