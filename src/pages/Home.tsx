34009ce04910eb2bf388aae2c812b7f9c3ca0df3
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-05-01 19:24:19 +0200

    Update Vibration mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,19 +8,16 @@ export default function App() {
     toast.info('Notification envoyée et affichée', {
       position: "top-right",
       autoClose: 5000,
-      hideProgressBar: false,
       closeOnClick: true,
     });
     console.log('Notification envoyée');
   }
 
   const handleClickVibration = () => {
-    // Vérifier si l'API Vibration est disponible dans le navigateur
     if ('vibrate' in navigator) {
-      // Faire vibrer l'appareil pendant 1000 ms
-      navigator.vibrate(1000);
+      navigator.vibrate(5000);
+      console.log('Vibration envoyée')
     } else {
-      // Si l'API Vibration n'est pas disponible, afficher un message d'erreur
       toast.error('L\'API Vibration n\'est pas disponible dans ce navigateur.');
     }
   }
