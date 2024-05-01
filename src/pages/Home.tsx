df9aae068437117435e833c4e9b49da78393c47c
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-05-01 19:36:10 +0200

    Add keybord for appel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import Header from '../components/header/Header';
-import '../style/Home.css'
+import '../style/Home.css';
 
 export default function App() {
+  const [phoneNumber, setPhoneNumber] = useState('');
 
   const handleClick = () => {
     toast.info('Notification envoyée et affichée', {
@@ -11,30 +13,53 @@ export default function App() {
       closeOnClick: true,
     });
     console.log('Notification envoyée');
-  }
+  };
 
   const handleClickVibration = () => {
     if ('vibrate' in navigator) {
       navigator.vibrate(5000);
-      console.log('Vibration envoyée')
+      console.log('Vibration envoyée');
     } else {
-      toast.error('L\'API Vibration n\'est pas disponible dans ce navigateur.');
+      toast.error("L'API Vibration n'est pas disponible dans ce navigateur.");
     }
-  }
+  };
 
   const handleCall = () => {
-    window.location.href = 'tel:';
+    if (phoneNumber) {
+      window.location.href = `tel:${phoneNumber}`;
+    } else {
+      toast.error("Veuillez entrer un numéro de téléphone.");
+    }
+  };
+
+  const handleKeyPress = (key: any) => {
+    setPhoneNumber(prevPhoneNumber => prevPhoneNumber + key);
+  };
+
+  const handleDelete = () => {
+    setPhoneNumber(prevPhoneNumber => prevPhoneNumber.slice(0, -1));
   };
 
   return (
-      <div>
-        <Header />
-        <h1>Vous êtes sur la page Home</h1>
-        <br />
-        <button onClick={handleClick}>Notification</button>
-        <button onClick={handleClickVibration}>Vibration</button>
-        <button onClick={handleCall}>Appeler</button>
+    <div>
+      <Header />
+      <h1>Vous êtes sur la page Home</h1>
+      <br />
+      <div className="phone-keypad">
+        <div className="phone-display">{phoneNumber}</div>
+        <div className="phone-keys">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, '*', 0, '#'].map(key => (
+            <button key={key} onClick={() => handleKeyPress(key)}>
+              {key}
+            </button>
+          ))}
+          <button onClick={handleDelete}>Effacer</button>
+        </div>
       </div>
+      <br />
+      <button onClick={handleClick}>Notification</button>
+      <button onClick={handleClickVibration}>Vibration</button>
+      <button onClick={handleCall}>Appeler</button>
+    </div>
   );
 }
-
