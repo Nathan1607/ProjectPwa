528cd005a3c3f5ed534f01eb1bedc0452533537e
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-05-01 19:37:19 +0200

    Fix bug deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import Header from '../components/header/Header';
 import '../style/Home.css';
