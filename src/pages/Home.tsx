9da73fb60e075a3e4c2b872426c98e9c07211c5b
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-05-01 19:16:31 +0200

    Test Vibration React

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,12 +14,24 @@ export default function App() {
     console.log('Notification envoyée');
   }
 
+  const handleClickVibration = () => {
+    // Vérifier si l'API Vibration est disponible dans le navigateur
+    if ('vibrate' in navigator) {
+      // Faire vibrer l'appareil pendant 1000 ms
+      navigator.vibrate(1000);
+    } else {
+      // Si l'API Vibration n'est pas disponible, afficher un message d'erreur
+      toast.error('L\'API Vibration n\'est pas disponible dans ce navigateur.');
+    }
+  }
+
   return (
       <div>
         <Header />
         <h1>Vous êtes sur la page Home</h1>
         <br />
         <button onClick={handleClick}>Notification</button>
+        <button onClick={handleClickVibration}>Vibration</button>
       </div>
   );
 }
