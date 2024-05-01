093e482a4ca7641b43cf5f5b4b88ffd5a3e9665e
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-05-01 18:57:46 +0200

    Test Notification Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,30 @@ import Header from '../components/header/Header';
 import '../style/Home.css'
 
 export default function App() {
+
+  const handleClick = () => {
+    if ('Notification' in window && Notification.permission === 'granted') {
+      console.log('Notification permission granted.')
+      new Notification('Titre de la notification', {
+        body: 'Contenu de la notification'
+      });
+    } else if ('Notification' in window && Notification.permission !== 'denied') {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          new Notification('Titre de la notification', {
+            body: 'Contenu de la notification'
+          });
+        }
+      });
+    }
+  }
+
   return (
       <div>
         <Header />
         <h1>Vous êtes sur la page Home</h1>
         <br />
-        <p>Test</p>
+        <button onClick={handleClick}>Notification</button>
       </div>
   );
 }
