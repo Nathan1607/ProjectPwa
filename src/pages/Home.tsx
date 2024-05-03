0cd063632369bdd2c5af2594cff6c56283dc5a0f
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-05-03 15:59:39 +0200

    Test async

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,32 +5,37 @@ import '../style/Home.css';
 export default function App() {
 
   useEffect(() => {
-    if ('OTPCredential' in window) {
-      console.log('WebOTP is supported')
-      
-        const input = document.querySelector('input[autocomplete="one-time-code"]');
-        if (!input) return;
-        const ac = new AbortController();
-        const form = input.closest('form');
-        if (form) {
-          form.addEventListener('submit', e => {
-            console.log(e);
-            ac.abort();
-          });
-        }
-        navigator.credentials.get({
-          otp: { transport:['sms'] },
-          signal: ac.signal,
-        }).then(otp => {
-          if (otp) {
-            (input as HTMLInputElement).value = otp.code;
-            if (form) form.submit();
+    async function OTP() {
+      if ('OTPCredential' in window) {
+        console.log('WebOTP is supported')
+        window.addEventListener('DOMContentLoaded', e => {
+          console.log(e);
+          const input = document.querySelector('input[autocomplete="one-time-code"]');
+          if (!input) return;
+          const ac = new AbortController();
+          const form = input.closest('form');
+          if (form) {
+            form.addEventListener('submit', e => {
+              console.log(e);
+              ac.abort();
+            });
           }
-        }).catch(err => {
-          console.log(err);
+          navigator.credentials.get({
+            otp: { transport:['sms'] },
+            signal: ac.signal,
+          }).then(otp => {
+            if (otp) {
+              (input as HTMLInputElement).value = otp.code;
+              if (form) form.submit();
+            }
+          }).catch(err => {
+            console.log(err);
+          });
         });
-
+      }
     }
+
+    OTP();
   }, []);
 
   return (
