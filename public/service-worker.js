ab3fc125f4f939c9ea8b1724877646622d2d5487
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-05-24 09:44:52 +0200

    update Service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,11 @@
-// Permet d'installer le service worker
-
 self.addEventListener("install", event => {
   console.log("Service Worker installing.");
 });
   
-// Permet d'activer le service worker
 self.addEventListener("activate", event => {
   console.log("Service Worker activating.");
 });
 
-// Gestion des notifications
 self.addEventListener('notificationclick', event => {
   console.log('Notification click Received.', event);
 
