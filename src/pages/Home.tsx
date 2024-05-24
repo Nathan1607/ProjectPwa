d32711aec063a13bea593c926bd4e82b1e42a5c3
Nathan Gaulard <77462468+Nathan1607@users.noreply.github.com>
2024-05-24 09:55:52 +0200

    Update QRCode Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import QRCode from 'qrcode.react';
 
 export default function App() {
 
-  const isntallURL = window.location.href.includes('localhost') ? 'http://localhost:5173' : 'https://nathan.gaulard.angers.mds-project.fr/';
+  const installUrl = 'https://nathan.gaulard.angers.mds-project.fr/';
 
   return (
     <div>
@@ -12,7 +12,7 @@ export default function App() {
       <h1>Vous êtes sur la page Home</h1>
       <br />
       <h2>Scannez le QR code pour installer l'application</h2>
-      <QRCode value={isntallURL} />
+      <QRCode value={installUrl} />
     </div>
   );
 }
